00edcbbf5eda6f36784cfd1f67b63700915a7555
João Nuno <49878220+BaTaTaAdb@users.noreply.github.com>
2022-06-29 13:53:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Enunciado completo disponível no LMS
+
+O teu programa deve dar ao utilizador uma forma de:
+  * Introduzir um novo supermercado na base de dados;
+  * Introduzir um novo produto na base de dados;
+  * Alterar a quantidade de determinado produto existente num determinado supermercado;
+  * Imprimir a lista de todos os produtos presentes num supermercado, e a respetiva quantidade de cada um. (Não deve imprimir os produtos que têm uma quantidade 0 disponível no supermercado);
+  * Imprimir a lista de supermercados em que um dado produto está disponível, e a quantidade disponível em cada um. (Não deve imprimir os supermercados em que há uma quantidade 0 de produto).
+'''
+
+def main():
+  input_1 input("")
+
+
+while True:
